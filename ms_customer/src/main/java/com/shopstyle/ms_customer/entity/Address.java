9dfce05b8fed2b0ea3adc1f329d0bbf84edacadf
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-10-21 13:59:11 -0300

    feat: entity address

---
+++ ms_customer/src/main/java/com/shopstyle/ms_customer/entity/Address.java
@@ -0,0 +1,44 @@
+package com.shopstyle.ms_customer.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"})
+@Entity
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "district", nullable = false)
+    private String district;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "complement", nullable = true)
+    private String complement;
+
+//    @Column(name = "customer", nullable = false)
+//    private Customer customer;
+
+}
