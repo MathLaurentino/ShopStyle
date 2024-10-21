5387c2d9ac8c25a3515417020fe4f4cba6ce5e56
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-10-21 16:04:14 -0300

    fix: CustomerPostDto

--- ms_customer/src/main/java/com/shopstyle/ms_customer/web/dto/CustomerPostDto.java
+++ ms_customer/src/main/java/com/shopstyle/ms_customer/web/dto/CustomerPostDto.java
@@ -13,13 +13,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class CustomerPostDto {
 
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "O CPF deve estar no formato xxx.xxx.xxx-xx")
     @NotBlank
     @CPF
     private String cpf;
 
+    @NotBlank
     @Size(min = 3)
     private String firstName;
 
+    @NotBlank
     @Size(min = 3)
     private String lastName;
 
@@ -28,6 +31,7 @@ public class CustomerPostDto {
     private String sex;
 
     @NotNull
+    @Past
     private LocalDate birthdate;
 
     @NotBlank
@@ -35,9 +39,11 @@ public class CustomerPostDto {
     private String email;
 
     @Size(min = 6)
+    @NotBlank
     private String password;
 
     @NotBlank
-    private boolean active;
+    @Pattern(regexp = "true|false")
+    private String active;
 
 }
