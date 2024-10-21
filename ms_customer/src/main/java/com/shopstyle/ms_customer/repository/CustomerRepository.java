403458e6f988f0fb4c2cbb07858fcb12c44e42a7
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-10-21 15:10:40 -0300

    feat: CustomerServiceImpl create Customer

--- ms_customer/src/main/java/com/shopstyle/ms_customer/repository/CustomerRepository.java
+++ ms_customer/src/main/java/com/shopstyle/ms_customer/repository/CustomerRepository.java
@@ -3,5 +3,12 @@ package com.shopstyle.ms_customer.repository;
 import com.shopstyle.ms_customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByCpf(String cpf);
+
+    Optional<Customer> findByEmail(String email);
+
 }
