e48bf0617a3f44e36ede127dfa908dae838e84ad
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-23 09:14:35 -0300

    feat: Sku

---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/entity/Sku.java
@@ -0,0 +1,43 @@
+package com.shopstyle.ms_catalog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"})
+@Entity
+public class Sku implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "color", nullable = false)
+    private String color;
+
+    @Column(name = "size", nullable = false)
+    private Float size;
+
+    @Column(name = "height", nullable = false)
+    private Float height;
+
+    @Column(name = "width", nullable = false)
+    private Float width;
+
+//    @ManyToOne
+//    @JoinColumn(name = "product_id", nullable = false)
+//    private Product product;
+
+}
