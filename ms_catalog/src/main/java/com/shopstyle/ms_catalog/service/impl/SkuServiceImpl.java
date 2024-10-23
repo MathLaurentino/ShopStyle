7c3933cf9402effd64e9e2447703b430384a0ccc
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-23 16:17:15 -0300

    feat: Sku service and controller implementation - create

---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/service/impl/SkuServiceImpl.java
@@ -0,0 +1,46 @@
+package com.shopstyle.ms_catalog.service.impl;
+
+import com.shopstyle.ms_catalog.entity.Product;
+import com.shopstyle.ms_catalog.entity.Sku;
+import com.shopstyle.ms_catalog.exception.EntityNotFoundException;
+import com.shopstyle.ms_catalog.repository.ProductRepository;
+import com.shopstyle.ms_catalog.repository.SkuRepository;
+import com.shopstyle.ms_catalog.service.SkuService;
+import com.shopstyle.ms_catalog.web.dto.SkuGetDto;
+import com.shopstyle.ms_catalog.web.dto.SkuPostDto;
+import com.shopstyle.ms_catalog.web.dto.SkuPutDto;
+import com.shopstyle.ms_catalog.web.dto.mapper.SkuMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SkuServiceImpl implements SkuService {
+
+    private final SkuRepository skuRepository;
+    private final ProductRepository productRepository;
+
+    @Override
+    public SkuGetDto createSku(SkuPostDto dto) {
+        Product product = productRepository.findById(dto.getProduct()).orElseThrow(
+                () -> new EntityNotFoundException("Product not found"));
+
+        Sku sku = SkuMapper.toSku(dto);
+        sku.setProduct(product);
+
+        return SkuMapper.toDto(skuRepository.save(sku));
+    }
+
+    //TODO
+    @Override
+    public SkuGetDto updateSku(SkuPutDto dto) {
+        return null;
+    }
+
+    //TODO
+    @Override
+    public void deleteSku(Long id) {
+
+    }
+
+}
---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/web/controller/impl/SkuControllerImpl.java
@@ -0,0 +1,42 @@
+package com.shopstyle.ms_catalog.web.controller.impl;
+
+import com.shopstyle.ms_catalog.service.SkuService;
+import com.shopstyle.ms_catalog.web.controller.SkuController;
+import com.shopstyle.ms_catalog.web.dto.SkuGetDto;
+import com.shopstyle.ms_catalog.web.dto.SkuPostDto;
+import com.shopstyle.ms_catalog.web.dto.SkuPutDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("v1/skus")
+@RestController
+public class SkuControllerImpl implements SkuController {
+
+    private final SkuService service;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<SkuGetDto> createSku(@Valid @RequestBody SkuPostDto dto) {
+        var getDto = service.createSku(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(getDto);
+    }
+
+    //TODO
+    @Override
+    public ResponseEntity<SkuGetDto> updateSku(@Valid @RequestBody SkuPutDto dto) {
+        return null;
+    }
+
+    //TODO
+    @Override
+    public ResponseEntity<Void> deleteSku(@PathVariable Long id) {
+        return null;
+    }
+
+}
