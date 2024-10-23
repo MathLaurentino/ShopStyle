c97a65b10aaf282e4781ca0e102ce4c361f0eb82
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-23 11:24:59 -0300

    feat: Category DTOs and Mapper

---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/web/dto/CategoryTreeDto.java
@@ -0,0 +1,14 @@
+package com.shopstyle.ms_catalog.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class CategoryTreeDto {
+    private Long id;
+    private String name;
+    private boolean active;
+    private List<CategoryTreeDto> subcategories; // Recursivamente mantém a lista de subcategorias
+}
