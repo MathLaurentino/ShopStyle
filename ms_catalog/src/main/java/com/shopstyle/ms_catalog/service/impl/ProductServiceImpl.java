6312c809bbd15924124ac8885aea3169cb061cb1
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-23 14:22:45 -0300

    feat: Product service and controller implementation - create

---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/service/impl/ProductServiceImpl.java
@@ -0,0 +1,60 @@
+package com.shopstyle.ms_catalog.service.impl;
+
+import com.shopstyle.ms_catalog.entity.Category;
+import com.shopstyle.ms_catalog.exception.CategoryHasChildrenException;
+import com.shopstyle.ms_catalog.exception.EntityNotFoundException;
+import com.shopstyle.ms_catalog.repository.CategoryRepository;
+import com.shopstyle.ms_catalog.repository.ProductRepository;
+import com.shopstyle.ms_catalog.service.ProductService;
+import com.shopstyle.ms_catalog.web.dto.ProductGetDto;
+import com.shopstyle.ms_catalog.web.dto.ProductReqDto;
+import com.shopstyle.ms_catalog.web.dto.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public ProductGetDto createProduct(ProductReqDto dto) {
+        Category category = categoryRepository.findById(dto.getCategory()).orElseThrow(
+                () -> new EntityNotFoundException("Category not found"));
+
+        if (!category.getSubcategories().isEmpty()) {
+            throw new CategoryHasChildrenException("Product cannot be added to category '" + category.getName() +
+                    "' because it has subcategories.");
+        }
+
+        var product = ProductMapper.toProduct(dto);
+        product.setCategory(category);
+
+        return ProductMapper.toDto(productRepository.save(product));
+    }
+
+    @Override
+    public ProductGetDto getProduct(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ProductGetDto> getAllProduct() {
+        return List.of();
+    }
+
+    @Override
+    public ProductGetDto updateProduct(ProductReqDto dto, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
+
+}
---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/web/controller/impl/ProductControllerImpl.java
@@ -0,0 +1,59 @@
+package com.shopstyle.ms_catalog.web.controller.impl;
+
+import com.shopstyle.ms_catalog.service.ProductService;
+import com.shopstyle.ms_catalog.web.controller.ProductController;
+import com.shopstyle.ms_catalog.web.dto.ProductGetDto;
+import com.shopstyle.ms_catalog.web.dto.ProductReqDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("v1/products")
+@RestController
+public class ProductControllerImpl implements ProductController {
+
+    private final ProductService service;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<ProductGetDto> createProduct(@Valid @RequestBody ProductReqDto dto) {
+        var getDto = service.createProduct(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(getDto);
+    }
+
+    //TODO
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductGetDto> getProduct(@PathVariable Long id) {
+        return null;
+    }
+
+    //TODO
+    @Override
+    @GetMapping
+    public ResponseEntity<List<ProductGetDto>> getAllProduct() {
+        return null;
+    }
+
+    //TODO
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductGetDto> updateProduct(@Valid @RequestBody ProductReqDto dto, @PathVariable Long id) {
+        return null;
+    }
+
+    //TODO
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        return null;
+    }
+
+}
