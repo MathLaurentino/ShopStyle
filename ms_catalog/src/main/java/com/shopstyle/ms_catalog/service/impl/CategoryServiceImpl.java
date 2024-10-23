d223ad360f0ec7c5407c4418202e79cbfbf04d82
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-23 11:33:24 -0300

    feat: category service and controller implementations - create category

---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,51 @@
+package com.shopstyle.ms_catalog.service.impl;
+
+import com.shopstyle.ms_catalog.repository.CategoryRepository;
+import com.shopstyle.ms_catalog.service.CategoryService;
+import com.shopstyle.ms_catalog.web.dto.CategoryGetDto;
+import com.shopstyle.ms_catalog.web.dto.CategoryReqDto;
+import com.shopstyle.ms_catalog.web.dto.CategoryTreeDto;
+import com.shopstyle.ms_catalog.web.dto.mapper.CategoryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository repository;
+
+    @Override
+    public CategoryGetDto createCategory(CategoryReqDto dto) {
+        var category = CategoryMapper.toCategory(dto);
+
+        if (dto.getParentCategory() != null) {
+            var parentCategory = repository.findById(dto.getParentCategory()).orElseThrow(
+                    () -> new RuntimeException("Category not found"));
+            category.setParentCategory(parentCategory);
+        }
+
+        return CategoryMapper.toDto(repository.save(category));
+    }
+
+    // TODO
+    @Override
+    public CategoryGetDto updateCategory(CategoryReqDto dto) {
+        return null;
+    }
+
+    // TODO
+    @Override
+    public List<CategoryTreeDto> getAllCategoriesAsTree() {
+        return List.of();
+    }
+
+    // TODO
+    @Override
+    public Void deleteCategoryById(Long id) {
+        return null;
+    }
+
+}
---
+++ ms_catalog/src/main/java/com/shopstyle/ms_catalog/web/controller/impl/CategoryControllerImpl.java
@@ -0,0 +1,47 @@
+package com.shopstyle.ms_catalog.web.controller.impl;
+
+import com.shopstyle.ms_catalog.service.CategoryService;
+import com.shopstyle.ms_catalog.web.controller.CategoryController;
+import com.shopstyle.ms_catalog.web.dto.CategoryGetDto;
+import com.shopstyle.ms_catalog.web.dto.CategoryReqDto;
+import com.shopstyle.ms_catalog.web.dto.CategoryTreeDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("v1/category")
+@RestController
+public class CategoryControllerImpl implements CategoryController {
+
+    private final CategoryService service;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<CategoryGetDto> createCategory(@Valid @RequestBody CategoryReqDto dto) {
+        var categoryDto = service.createCategory(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryDto);
+    }
+
+    @Override
+    public ResponseEntity<CategoryGetDto> updateCategory(@Valid @RequestBody CategoryReqDto dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<CategoryTreeDto>> getAllCategoriesAsTree() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id) {
+        return null;
+    }
+
+}
