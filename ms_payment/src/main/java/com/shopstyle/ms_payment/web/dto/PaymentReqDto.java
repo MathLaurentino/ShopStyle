13666c854940e774a553b1834e13db6340949980
Matheus Laurentino <55161637+MathLaurentino@users.noreply.github.com>
2024-10-28 09:38:04 -0300

    feat: add ExceptionHandler

--- ms_payment/src/main/java/com/shopstyle/ms_payment/web/dto/PaymentReqDto.java
+++ ms_payment/src/main/java/com/shopstyle/ms_payment/web/dto/PaymentReqDto.java
@@ -2,6 +2,7 @@ package com.shopstyle.ms_payment.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,9 @@ public class PaymentReqDto {
     @NotBlank
     private String type;
 
-    @NotNull
-    private Boolean installment;
+    @NotBlank
+    @Pattern(regexp = "true|false")
+    private String installments;
 
     @NotNull
     private Boolean active;
